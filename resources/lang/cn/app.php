2385ee424e6b6a98713bbf384a92923d5f9dd068
田闯 <1509760688@qq.com>
2019-11-29 23:09:15 +0800

    汉化

---
+++ resources/lang/cn/app.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Login' => '登陆',
+    'E-Mail Address' => '邮箱地址',
+    'Password' => '密码',
+    'Remember Me' => '记住我',
+    'Forgot Your Password?' => '忘记密码',
+    'Register' => '注册',
+    'Name' => '名字',
+    'Confirm Password' => '确认密码',
+//    'Password' => '密码',
+//    'Remember Me' => '记住我',
+//    'Forgot Your Password?' => '忘记密码',
+//    'Register' => '注册',
+
+
+
+];
