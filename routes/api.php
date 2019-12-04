a2883a68ac214720dde4741bd5600c09b8cc41c9
田闯 <1509760688@qq.com>
2019-12-04 22:49:17 +0800

    jjj

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/update/updateImage', 'api\ApiController@updateImage');
+Route::post('/update/updateImage', 'Api\ApiController@updateImage');
