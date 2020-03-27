0fd28b42fbb6dcf29ed2e9143b60c9014f81d536
tianchuang <1509760688@qq.com>
2020-03-27 16:07:02 +0800

    no message

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,6 +70,11 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path(''),
+            'url' => '',
+        ],
 
     ],
 
